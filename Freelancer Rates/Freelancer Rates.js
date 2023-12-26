aa588375789afec1447fd93ecbf8163a7d1a3725
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2023-12-26 21:56:48 +0700

    Update Freelancer Rates.js

--- Freelancer Rates/Freelancer Rates.js
+++ Freelancer Rates/Freelancer Rates.js
@@ -6,8 +6,9 @@
  */
 const workHoursPerDay = 8;
 const billableDaysPerMonth = 22;
+
 export function dayRate(ratePerHour) {
-  return workHoursPerDay*ratePerHour;
+  return workHoursPerDay * ratePerHour;
 }
 
 /**
@@ -18,8 +19,8 @@ export function dayRate(ratePerHour) {
  * @returns {number} the number of days
  */
 export function daysInBudget(budget, ratePerHour) {
-    return Math.floor(budget/dayRate(ratePerHour));
-  }
+  return Math.floor(budget / dayRate(ratePerHour));
+}
 
 /**
  * Calculates the discounted rate for large projects, rounded up
@@ -30,9 +31,10 @@ export function daysInBudget(budget, ratePerHour) {
  * @returns {number} the rounded up discounted rate
  */
 export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
-    let monthlyRate = (billableDaysPerMonth*dayRate(ratePerHour)) * (1-discount);
-    let numberOfMonth = Math.floor(numDays/billableDaysPerMonth);
-    let totalChargableDays = (numDays%22)*dayRate(ratePerHour);
-    return Math.ceil((monthlyRate*numberOfMonth)+totalChargableDays);
-    
-  }
+  let monthlyRate = (billableDaysPerMonth * dayRate(ratePerHour)) * (1 - discount);
+  let numberOfMonth = Math.floor(numDays / billableDaysPerMonth);
+  let totalChargableDays = (numDays % billableDaysPerMonth) * dayRate(ratePerHour);
+
+  return Math.ceil(monthlyRate * numberOfMonth + totalChargableDays);
+  
+}
