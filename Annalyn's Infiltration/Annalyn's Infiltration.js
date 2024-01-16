da3f931b7219490e33a228484e1eb6809f69f392
Andryan Rifqi Irsyad <andryan.rifqi@quipper.com>
2024-01-16 22:16:37 +0700

    Update Annalyn's Infiltration.js

--- Annalyn's Infiltration/Annalyn's Infiltration.js
+++ Annalyn's Infiltration/Annalyn's Infiltration.js
@@ -29,3 +29,16 @@
 export function canExecuteFastAttack(knightIsAwake) {
     return !knightIsAwake;
   }
+
+  /**
+ * A useful spy captures information, which they can't do if everyone's asleep.
+ *
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ *
+ * @returns {boolean} Whether or not you can spy on someone.
+ */
+export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+    return knightIsAwake || archerIsAwake || prisonerIsAwake;
+  }
